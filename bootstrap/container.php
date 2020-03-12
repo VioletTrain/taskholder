0423ef542041152065859e83a754a5d6d4faab48
Andrij Kshanovskij <andrewafk2000@gmail.com>
2020-03-12 12:20:19 +0200

    Install Twig

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -6,16 +6,28 @@ use Doctrine\ORM\Tools\Setup;
 use Framework\Container;
 use Framework\Contract\EntityManager as CustomEntityManagerInterface;
 use Framework\EntityManager as CustomEntityManager;
+use Twig\Environment;
+use Twig\Extension\DebugExtension;
+use Twig\Loader\FilesystemLoader;
 
 $container = new Container();
 
 $container->singleton(Container::class, Container::class);
+$container->singleton(CustomEntityManagerInterface::class, CustomEntityManager::class);
+
 $container->singleton(EntityManagerInterface::class, function () {
     $config = include(BASE_PATH . '/config/db.php');
     $doctrineConfig = Setup::createAnnotationMetadataConfiguration($config['entity_paths'], $config['dev_mode']);
 
     return EntityManager::create($config['db_params'], $doctrineConfig);
 });
-$container->singleton(CustomEntityManagerInterface::class, CustomEntityManager::class);
+
+$container->bind(Environment::class, function () {
+    $loader = new FilesystemLoader(BASE_PATH . '/template');
+    $twig = new Environment($loader, ['debug' => true]);
+    $twig->addExtension(new DebugExtension());
+
+    return $twig;
+});
 
 return $container;
